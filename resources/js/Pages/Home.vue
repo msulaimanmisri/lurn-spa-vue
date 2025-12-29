b19134733400207dae17f9f0900459ac32caaf1d
Sulaiman Misri <msulaimanmisri@gmail.com>
2025-12-29 08:27:19 +0800

    WIP

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+
+    <Head title="Homepage" />
+
+    <div class="bg-gray-50 text-black/50">
+        <div class="flex">
+            <Link :href="route('page.index')">
+                <button class="bg-red-500 px-6 py-3 rounded-md shadow-md m-6">
+                    Login
+                </button>
+            </Link>
+        </div>
+    </div>
+</template>
