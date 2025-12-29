4e44bd9e64f135a8da0ba2c115c1a5bd44e3b245
Sulaiman Misri <msulaimanmisri@gmail.com>
2025-12-29 10:47:20 +0800

    change from NGINX to FrankenPHP

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -7,9 +7,13 @@
     <Head title="Homepage" />
 
     <div class="bg-gray-50 text-black/50 p-6">
-        <div class="flex">
+        <div class="flex gap-2">
             <Link :href="route('login')" class="bg-red-500 text-white rounded-lg py-3 px-8">
                 Login
+            </Link>
+
+            <Link :href="route('register')" class="bg-red-500 text-white rounded-lg py-3 px-8">
+                Register
             </Link>
         </div>
     </div>
